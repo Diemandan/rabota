85e3e432a5bfbfbb5c4601bfe09edadb797d7342
diemandan <diemandan63@gmail.com>
2024-11-16 20:17:50 +0300

    fixes

--- app/Http/Controllers/API/PaymentController.php
+++ app/Http/Controllers/API/PaymentController.php
@@ -7,9 +7,11 @@ use Illuminate\Support\Facades\Log;
 
 class PaymentController
 {
-public function saveSuccessPaymentInfo(Request $request)
-{
-    $data = $request->all();
-    Log::info('УСПЕШНО' . json_encode($data));
-}
+    public function saveSuccessPaymentInfo(Request $request)
+    {
+        $data = $request->all();
+        Log::info('УСПЕШНО' . json_encode($data));
+
+        return response()->json(['success' => true], 200);
+    }
 }
