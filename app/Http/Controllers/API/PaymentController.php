83c1931251690ec7ea386fd01e39a33da8d468ad
diemandan <diemandan63@gmail.com>
2024-11-16 20:14:38 +0300

    bank callback

---
+++ app/Http/Controllers/API/PaymentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class PaymentController
+{
+public function saveSuccessPaymentInfo(Request $request)
+{
+    $data = $request->all();
+    Log::info('УСПЕШНО' . json_encode($data));
+}
+}
